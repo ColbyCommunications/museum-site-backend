36274e84333b957c8769acbfb92ca7f7dd121bd4
Brandon K. Waltz <bwaltz6@gmail.com>
2025-04-23 10:46:52 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -148,8 +148,9 @@ if (false !== $strRelationships = getenv('PLATFORM_RELATIONSHIPS')) {
 }
 
 // Define wp-content directory outside of WordPress core directory
-define('WP_HOME', $site_scheme . '://' . $site_host);
-define('WP_SITEURL', WP_HOME . '/wp');
+define('WP_HOME', 'https://colby-museum-frontend.pages.dev');
+define('WP_SITEURL', $site_scheme . '://' . $site_host . '/wp');
+
 
 define('WP_CONTENT_DIR', dirname(__FILE__) . '/web/wp-content');
 
