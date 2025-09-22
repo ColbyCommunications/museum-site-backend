ceeb4e7ad43bd403ca38f416dcfa651fc1f57ce1
Brandon K. Waltz <bwaltz6@gmail.com>
2025-09-22 10:58:07 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -124,6 +124,9 @@ if (false !== $strRelationships = getenv('PLATFORM_RELATIONSHIPS')) {
     // in a wp-config-local.php file to skip this setting on local development.
     if (!defined('WP_DEBUG')) {
         define('WP_DEBUG', false);
+        ini_set( 'display_errors', 'Off' );
+		ini_set( 'error_reporting', E_ALL );
+		define( 'WP_DEBUG_DISPLAY', false );
     }
 
     // Set all of the necessary keys to unique values, based on the Platform.sh
