a9992d7b5f2b507083e335441355f37e5ba3d284
Brandon K. Waltz <bwaltz6@gmail.com>
2025-11-14 12:55:42 -0500

    Update admin-tools.js

--- web/wp-content/themes/twentytwentythree/js/admin-tools.js
+++ web/wp-content/themes/twentytwentythree/js/admin-tools.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
     // Target trash links in the page/post list
-    $('a.submitdelete').on('click', function (e) {
+    $('a.np-btn-trash').on('click', function (e) {
         if (!confirm('Are you sure you want to move this item to the trash?')) {
             e.preventDefault(); // Stop the link from being followed
         }
