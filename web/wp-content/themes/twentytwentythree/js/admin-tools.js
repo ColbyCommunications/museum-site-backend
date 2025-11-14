7ba62644a0ce956e383a5248f551e9eb9b61906f
Brandon K. Waltz <bwaltz6@gmail.com>
2025-11-14 13:12:13 -0500

    Update admin-tools.js

--- web/wp-content/themes/twentytwentythree/js/admin-tools.js
+++ web/wp-content/themes/twentytwentythree/js/admin-tools.js
@@ -5,17 +5,4 @@ jQuery(document).ready(function ($) {
             e.preventDefault(); // Stop the link from being followed
         }
     });
-
-    // Target "Move to trash" link in the block editor
-    // This selector is more complex and can change with WP versions
-    $(document).on(
-        'click',
-        '.editor-post-trash button, .components-button.editor-post-trash',
-        function (e) {
-            if (!confirm('Are you sure you want to move this item to the trash?')) {
-                e.preventDefault(); // Stop the action
-                return false;
-            }
-        }
-    );
 });
